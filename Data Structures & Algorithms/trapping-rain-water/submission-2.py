8d168163f15638547927279355b9cf5d8da76e5b
Aditya-Bhadoo <33895184+Aditya-Bhadoo@users.noreply.github.com>
2026-04-28 18:48:24 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def trap(self, ht: List[int]) -> int:
+        # start from the beginning and look for anything above 0
+        # when you get a hit, do this until you get something higher(this will be the next hit) than that hit
+        # calculate the difference between that height and the hit
+        lmax=[0 for _ in range(len(ht))]
+        rmax=[0 for _ in range(len(ht))]
+
+        lcur=0
+        for l in range(len(ht)):
+            lcur = max(ht[l],lcur)
+            lmax[l]=lcur
+        hcur=0
+        for r in range(len(ht)-1,-1,-1):
+            hcur=max(ht[r],hcur)
+            rmax[r] = hcur
+        twater=0
+        for i in range(len(ht)):
+            water=min(lmax[i],rmax[i])-ht[i]
+            if water < 0:
+                print("WTF")
+            twater+=water
+
+        print(lmax)
+        print(rmax)
+
+        return twater
